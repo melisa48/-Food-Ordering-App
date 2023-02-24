6039226e6d0e4906da3814b676894838a4a44ff2
emhuang3 <emilyhuangx2012@gmail.com>
2023-02-23 21:44:43 -0800

    completed basic skeleton about and member pages

--- application/app.js
+++ application/app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var andyRouter = require('./routes/andy');
+var juandavidRouter = require('./routes/juandavid');
+var emilyRouter = require('./routes/emily');
+var euniceRouter = require('./routes/eunice');
+var melisaRouter = require('./routes/melisa');
 
 var app = express();
 
@@ -20,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/andy', andyRouter);
+app.use('/juandavid', juandavidRouter);
+app.use('/emily', emilyRouter);
+app.use('/eunice', euniceRouter);
+app.use('/melisa', melisaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
