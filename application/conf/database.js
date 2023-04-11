763a066fc238677c579f9f3c5126a8ebc41efe6a
emhuang3 <emilyhuangx2012@gmail.com>
2023-04-11 10:57:52 -0700

    updated navbar links and fixed navbar UI

--- application/conf/database.js
+++ application/conf/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: '34.217.102.90',
+    host: '34.219.117.242',
     user: 'team7',
     database: 'team7',
     password: 'Backside180!'
