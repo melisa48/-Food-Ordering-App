40918fdf552e599b404edbcffa7fc58d8fee515b
Eunice Borres <euborres@gmail.com>
2023-04-23 23:32:50 -0700

    installed bcrypt package for hashing passwords. Added password hashing and email validation for user registration

--- application/routes/registration-routes/sfsuRegistration.js
+++ application/routes/registration-routes/sfsuRegistration.js
@@ -1,34 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../../conf/database");
+var bcrypt = require('bcrypt');
+
 router.get('/', function(req, res, next) {
   res.render('registration/sfsuRegistration', {title: 'SFSU Registration'});
 });
-
-router.post('/sfsuLogin',(req, res, next) => {
+//Author: Eunice
+router.post('/sfsuRegister',(req, res, next) => {
   // console.log('registration router reached.');
   let firstname = req.body.firstname;
   let lastname = req.body.lastname;
   let email = req.body.email;
   let password = req.body.password;
+  let confirmpassword = req.body.confirmpassword;
   //Tutorial Used: https://www.youtube.com/watch?v=auEkP8ZKWOE 
-  db.query("SELECT * FROM registeredUsers WHERE verifiedEmail = ?",[email],function(err,result,fields){
-    if(err) throw err;
+  if(confirmpassword == password){
+    if(isValid(email)){
+      db.query("SELECT * FROM registeredUsers WHERE verifiedEmail = ?",[email],function(err,result,fields){
 
-    if(result.length > 0 ){
-      console.log("Email is already registered.");
-    }else{
-      let baseSQL = "INSERT INTO registeredUsers(firstname, lastname, password, verifiedEmail) VALUES (?,?,?,?)";
-      db.query(baseSQL, [firstname, lastname, password, email], function(err, result, fields){
         if(err) throw err;
-        console.log("Supposed to be redirecting to login\n");
-        // res.redirect('/');
+        //verifying that the passwords are the same
+  
+          if(result.length > 0 ){
+            console.log("Email is already registered.");
+          }else{
+            var hashpassword = bcrypt.hashSync(password,10);
+            console.log(hashpassword);
+            let baseSQL = "INSERT INTO registeredUsers(firstname, lastname, password, verifiedEmail) VALUES (?,?,?,?)";
+            db.query(baseSQL, [firstname, lastname, hashpassword, email], function(err, result, fields){
+              if(err) throw err;
+              console.log("Supposed to be redirecting to login\n");
+              res.redirect('/sfsuLogin');
+            })
+          }
+          
       })
+    }else{
+      console.log("Not a valid email address.\n");
+      res.redirect('/sfsuRegistration');
     }
-  })
-  res.redirect('/sfsuLogin');
+    
+  }else{
+    console.log("Passwords do not match\n");
+    res.redirect('/sfsuRegistration');
+  }
 
 });
-  
+
+function isValid(email){
+  var parts = email.split("@");
+  if(parts[1] === "sfsu.edu" || parts[1] === "mail.sfsu.edu"){
+    return true;
+  }
+  return false;
+}
   
 module.exports = router;
