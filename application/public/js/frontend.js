dd2278c99c18f36515fc5b8587d89955f86ef3f6
emhuang3 <emilyhuangx2012@gmail.com>
2023-04-25 16:20:01 -0700

    added find food button to home page

--- application/public/js/frontend.js
+++ application/public/js/frontend.js
@@ -1,9 +1,16 @@
-// Code to submit search form (search bar in navbar)
+// Code to submit search form (search bar in navbar or home page find food button) 
 // Author(s): Emily
 var searchButton = document.getElementById('search-button');
 var searchForm = document.getElementById("search-form");
+var searchButton2 = document.getElementById('find-food-button');
 
 searchButton.addEventListener("click", function(e){
+    e.preventDefault();
+    searchForm.submit();
+    
+});
+
+searchButton2.addEventListener("click", function(e){
     e.preventDefault();
     searchForm.submit();
     
