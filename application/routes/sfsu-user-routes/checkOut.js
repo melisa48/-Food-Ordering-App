03ffdee1eb2eb2830aa10ad984bbe9d7a1901266
emhuang3 <emilyhuangx2012@gmail.com>
2023-05-03 21:27:10 -0700

    made checkout food items deletable

--- application/routes/sfsu-user-routes/checkOut.js
+++ application/routes/sfsu-user-routes/checkOut.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('sfsu-user-pages/checkOut', {title: 'checkOut'});
+  res.render('sfsu-user-pages/checkOut',{
+    title: 'checkOut',
+    array: [0,2,3,4]
+  });
 });
 module.exports = router;
